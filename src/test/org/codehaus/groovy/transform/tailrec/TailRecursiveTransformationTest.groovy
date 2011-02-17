669d817c6ea4201f3c1d9270810f9f8b155c0610
Johannes Link <business@johanneslink.net>
2011-02-17 13:25:10 +0100

    Added basic classes and packages

---
+++ src/test/org/codehaus/groovy/transform/tailrec/TailRecursiveTransformationTest.groovy
@@ -0,0 +1,11 @@
+package org.codehaus.groovy.transform.tailrec
+
+
+import groovy.util.GroovyTestCase;
+
+class TailRecursiveTransformationTest extends GroovyTestCase {
+
+	void testShouldFail() {
+		fail()
+	}
+}
