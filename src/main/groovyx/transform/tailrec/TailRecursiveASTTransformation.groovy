34638b39ba85ad804d750839c07f71c5567ad610
Johannes Link <business@johanneslink.net>
2011-02-23 21:41:17 +0100

    Added first test for annotated but non-recursive methods

--- src/main/groovyx/transform/tailrec/TailRecursiveASTTransformation.groovy
+++ src/main/groovyx/transform/tailrec/TailRecursiveASTTransformation.groovy
@@ -1,6 +1,8 @@
 package groovyx.transform.tailrec
 
 
+import groovyx.transform.TailRecursive
+
 import org.codehaus.groovy.ast.*
 import org.codehaus.groovy.ast.expr.*
 import org.codehaus.groovy.ast.stmt.*
@@ -10,7 +12,30 @@ import org.codehaus.groovy.transform.*
 @GroovyASTTransformation(phase=CompilePhase.SEMANTIC_ANALYSIS)
 class TailRecursiveASTTransformation implements ASTTransformation {
 
+	private static final Class MY_CLASS = TailRecursive.class;
+	private static final ClassNode MY_TYPE = new ClassNode(MY_CLASS);
+	static final String MY_TYPE_NAME = "@" + MY_TYPE.getNameWithoutPackage()
+
 	@Override
 	public void visit(ASTNode[] nodes, SourceUnit source) {
+		if (!nodes) return;
+		if (!nodes[0]) return;
+		if (!nodes[1]) return;
+		if (!(nodes[0] instanceof AnnotationNode)) return;
+		if (!(nodes[1] instanceof MethodNode)) return;
+
+		MethodNode method = nodes[1];
+		if (!hasRecursiveMethodCalls(method)) {
+			println(transformationDescription(method) + " skipped: No recursive calls detected.")
+			return;
+		}
+	}
+
+	def transformationDescription(MethodNode method) {
+		"$MY_TYPE_NAME transformation on method '${method.name}(${method.parameters.size()} params)'"
+	}
+
+	boolean hasRecursiveMethodCalls(MethodNode method) {
+		false
 	}
 }
--- src/test/groovyx/transform/tailrec/TailRecursiveTransformationTest.groovy
+++ src/test/groovyx/transform/tailrec/TailRecursiveTransformationTest.groovy
@@ -1,11 +1,33 @@
 package groovyx.transform.tailrec
 
+class TailRecursiveTransformationTest extends GroovyShellTestCase {
 
-import groovy.util.GroovyTestCase;
-
-class TailRecursiveTransformationTest extends GroovyTestCase {
-
-	void testShouldFail() {
-		fail()
+	void testIgnoreMethodsWithoutRecursiveCall() {
+		def target = evaluate("""
+            import groovyx.transform.TailRecursive
+            class TargetClass {
+            	@TailRecursive
+            	void aVoidMethod() {
+            		new Object()
+            	}
+				@TailRecursive
+				static void aStaticVoidMethod() {
+					new Object()
+				}
+				@TailRecursive
+				int aFunction() {
+					42
+				}
+				@TailRecursive
+				static int aStaticFunction() {
+					43
+				}
+            }
+            new TargetClass()
+        """)
+		target.aVoidMethod()
+		target.aStaticVoidMethod()
+		assert target.aFunction() == 42
+		assert target.aStaticFunction() == 43
 	}
 }
