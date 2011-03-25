a94060ebb058000bd8502d0c3cba2e934a9ea3d6
Johannes Link <business@johanneslink.net>
2011-03-25 19:13:09 +0100

    Some rudimentary java doc added to TailRecursive

--- src/main/groovyx/transform/TailRecursive.groovy
+++ src/main/groovyx/transform/TailRecursive.groovy
@@ -4,6 +4,26 @@ package groovyx.transform
 import java.lang.annotation.*
 import org.codehaus.groovy.transform.GroovyASTTransformationClass
 
+/**
+ * Class annotation used to transform method with tail recursive calls into iterative methods automagically
+ * since the JVM cannot do this itself. This works for both static and non-static methods.
+ * <p/>
+ * It allows you to write a method like this:
+ * <pre>
+ *  {@code @TailRecursive} 
+ *  long sumUp(long number, long sum) {
+ *    if (number == 0)
+ *      return sum;
+ *    sumUp(number - 1, sum + number)
+ *  }
+ * </pre>
+ * Know shortcomings:
+ * <ul>
+ * <li>Only non-void methods are currently being handled.
+ * <li>The recursive call must not use default parameters.
+ * <li>Many special cases (eg. returns of ternary operator) are not yet being handled.
+ * </ul>
+*/
 @Retention(RetentionPolicy.SOURCE)
 @Target([ElementType.METHOD])
 @GroovyASTTransformationClass(["groovyx.transform.tailrec.TailRecursiveASTTransformation"])
