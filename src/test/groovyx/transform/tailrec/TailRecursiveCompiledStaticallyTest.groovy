3069a70f4a34added592fbd4c1f32c1bc951a126
Johannes Link <business@johanneslink.net>
2014-01-17 15:25:20 +0100

    Added one more test for statically compiled tail recursive method

--- src/test/groovyx/transform/tailrec/TailRecursiveCompiledStaticallyTest.groovy
+++ src/test/groovyx/transform/tailrec/TailRecursiveCompiledStaticallyTest.groovy
@@ -1,11 +1,9 @@
 package groovyx.transform.tailrec
 
-import groovy.transform.TypeChecked
-
 class TailRecursiveCompiledStaticallyTest extends GroovyShellTestCase {
 
-	void testStaticallyCompiledRecursiveMethod() {
-		def target = evaluate("""
+    void testStaticallyCompiledRecursiveMethod() {
+        def target = evaluate("""
             import groovyx.transform.TailRecursive
             import groovy.transform.CompileStatic
 
@@ -20,9 +18,9 @@ class TailRecursiveCompiledStaticallyTest extends GroovyShellTestCase {
             }
             new TargetClass()
         """)
-		assert target.staticCountDown(5) == 0
-		assert target.staticCountDown(100000) == 0
-	}
+        assert target.staticCountDown(5) == 0
+        assert target.staticCountDown(100000) == 0
+    }
 
     void testTypeCheckedRecursiveMethod() {
         def target = evaluate("""
@@ -43,4 +41,28 @@ class TailRecursiveCompiledStaticallyTest extends GroovyShellTestCase {
         assert target.staticCountDown(5) == 0
         assert target.staticCountDown(100000) == 0
     }
+
+    void testRecursiveFunctionWithTwoParameters() {
+        def target = evaluate('''
+            import groovyx.transform.TailRecursive
+            import groovy.transform.CompileStatic
+
+            @CompileStatic
+            class TargetClass {
+				@TailRecursive
+				String fillString(long number, String filled) {
+					if (number == 0)
+						return filled;
+					fillString(number - 1, filled + "+")
+				}
+        	}
+            new TargetClass()
+		''')
+
+        assert target.fillString(0, "") == ""
+        assert target.fillString(1, "") == "+"
+        assert target.fillString(5, "") == "+++++"
+        assert target.fillString(10000, "") == "+" * 10000
+    }
+
 }
