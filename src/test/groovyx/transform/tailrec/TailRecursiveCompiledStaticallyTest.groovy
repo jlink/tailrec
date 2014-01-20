6a5ea9f3f0a1bac12192c8c8dd24072c14a34260
Johannes Link <business@johanneslink.net>
2014-01-20 11:37:01 +0100

    Added test for static compilation failure in tail recursive method

--- src/test/groovyx/transform/tailrec/TailRecursiveCompiledStaticallyTest.groovy
+++ src/test/groovyx/transform/tailrec/TailRecursiveCompiledStaticallyTest.groovy
@@ -1,5 +1,7 @@
 package groovyx.transform.tailrec
 
+import org.codehaus.groovy.control.CompilationFailedException
+
 class TailRecursiveCompiledStaticallyTest extends GroovyShellTestCase {
 
     void testStaticallyCompiledRecursiveMethod() {
@@ -22,27 +24,48 @@ class TailRecursiveCompiledStaticallyTest extends GroovyShellTestCase {
         assert target.staticCountDown(100000) == 0
     }
 
-    void testTypeCheckedRecursiveMethod() {
-        def target = evaluate("""
+    void testRecursiveMethodWhichCannotBeStaticallyCompiled() {
+        shouldFail(CompilationFailedException) { evaluate("""
             import groovyx.transform.TailRecursive
-            import groovy.transform.TypeChecked
+            import groovy.transform.CompileStatic
 
-            @TypeChecked
+            @CompileStatic
             class TargetClass {
             	@TailRecursive
-            	static int staticCountDown(int zahl) {
+            	static int staticCountDown(zahl) {
             		if (zahl == 0)
             			return zahl
             		return staticCountDown(zahl - 1)
             	}
             }
             new TargetClass()
-        """)
-        assert target.staticCountDown(5) == 0
-        assert target.staticCountDown(100000) == 0
+        """) }
     }
 
-    void testRecursiveFunctionWithTwoParameters() {
+    void testTypeCheckedRecursiveMethod() {
+        def target = evaluate('''
+            import groovyx.transform.TailRecursive
+            import groovy.transform.TypeChecked
+
+            @TypeChecked
+            class TargetClass {
+				@TailRecursive
+				String fillString(long number, String filled) {
+					if (number == 0)
+						return filled;
+					fillString(number - 1, filled + "+")
+				}
+        	}
+            new TargetClass()
+		''')
+
+        assert target.fillString(0, "") == ""
+        assert target.fillString(1, "") == "+"
+        assert target.fillString(5, "") == "+++++"
+        assert target.fillString(10000, "") == "+" * 10000
+    }
+
+    void testStaticallyCompiledRecursiveFunctionWithTwoParameters() {
         def target = evaluate('''
             import groovyx.transform.TailRecursive
             import groovy.transform.CompileStatic
