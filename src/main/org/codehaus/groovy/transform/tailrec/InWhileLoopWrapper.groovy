3a6a13019a7154e05aaa269c065e399aa198c47a
Johannes Link <business@johanneslink.net>
2014-01-27 15:28:21 +0100

    Creating a new varaiable scope with the old one as parent.

--- src/main/org/codehaus/groovy/transform/tailrec/InWhileLoopWrapper.groovy
+++ src/main/org/codehaus/groovy/transform/tailrec/InWhileLoopWrapper.groovy
@@ -1,6 +1,7 @@
 package org.codehaus.groovy.transform.tailrec
 
 import org.codehaus.groovy.ast.MethodNode
+import org.codehaus.groovy.ast.VariableScope
 import org.codehaus.groovy.ast.expr.BooleanExpression
 import org.codehaus.groovy.ast.expr.ConstantExpression
 import org.codehaus.groovy.ast.stmt.BlockStatement
@@ -15,7 +16,7 @@ class InWhileLoopWrapper {
 		WhileStatement whileLoop = new WhileStatement(new BooleanExpression(new ConstantExpression(true)), oldBody)
 		if (whileLoop.loopBlock.statements.size() > 0)
 			whileLoop.loopBlock.statements[0].statementLabel = LOOP_LABEL
-		BlockStatement newBody = new BlockStatement([], method.variableScope)
+		BlockStatement newBody = new BlockStatement([], new VariableScope(method.variableScope))
 		newBody.addStatement(whileLoop)
 		method.code = newBody
 	}
