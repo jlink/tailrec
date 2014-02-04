44af05cc561ce0df1b39e4670c394c55594f25cf
Johannes Link <business@johanneslink.net>
2014-02-04 21:28:31 +0100

    First steps towards VariableExpressionReplacer

---
+++ src/main/org/codehaus/groovy/transform/tailrec/VariableExpressionReplacer.groovy
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.transform.tailrec
+
+import groovy.transform.CompileStatic
+import org.codehaus.groovy.ast.ASTNode
+import org.codehaus.groovy.ast.CodeVisitorSupport
+import org.codehaus.groovy.ast.expr.ClosureExpression
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.ast.expr.ExpressionTransformer
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codehaus.groovy.ast.stmt.*
+
+import java.lang.reflect.Method
+
+/**
+ * Tool for replacing VariableExpression instances in an AST by other VariableExpression instances.
+ *
+ * Within @TailRecursive it is used to swap the access of arg with the access of temp vars
+ *
+ * @author Johannes Link
+ */
+@CompileStatic
+class VariableExpressionReplacer extends CodeVisitorSupport {
+
+    Closure<Boolean> when = { VariableExpression node -> false }
+    Closure<VariableExpression> replaceWith = { VariableExpression variableExpression -> variableExpression }
+
+    private ExpressionTransformer transformer
+
+    synchronized void replaceIn(ASTNode root) {
+        transformer = new VariableExpressionTransformer(when: when, replaceWith: replaceWith)
+        root.visit(this)
+    }
+
+    public void visitReturnStatement(ReturnStatement statement) {
+        replaceExpressionPropertyWhenNecessary(statement)
+        super.visitReturnStatement(statement);
+    }
+
+    private void replaceExpressionPropertyWhenNecessary(ASTNode node, String propName = "expression") {
+        //Simulate Groovy's property access
+        String getterName = 'get' + propName[0].toUpperCase() + propName[1..-1]
+        String setterName = 'set' + propName[0].toUpperCase() + propName[1..-1]
+        Method getExpressionMethod = node.class.getMethod(getterName, new Class[0])
+        Method setExpressionMethod = node.class.getMethod(setterName, [Expression].toArray(new Class[1]))
+        Expression expr = getExpressionMethod.invoke(node, new Object[0]) as Expression
+
+        if (expr instanceof VariableExpression) {
+            if (when(expr)) {
+                VariableExpression newExpr = replaceWith(expr)
+                setExpressionMethod.invoke(node, [newExpr].toArray())
+            }
+        } else {
+            Expression newExpr = expr.transformExpression(transformer)
+            setExpressionMethod.invoke(node, [newExpr].toArray())
+        }
+    }
+
+
+    public void visitIfElse(IfStatement ifElse) {
+        super.visitIfElse(ifElse);
+    }
+
+    public void visitForLoop(ForStatement forLoop) {
+        super.visitForLoop(forLoop);
+    }
+
+    public void visitWhileLoop(WhileStatement loop) {
+        super.visitWhileLoop(loop);
+    }
+
+    public void visitDoWhileLoop(DoWhileStatement loop) {
+        super.visitDoWhileLoop(loop);
+    }
+
+}
+
+@CompileStatic
+class VariableExpressionTransformer implements ExpressionTransformer {
+
+    Closure<Boolean> when
+    Closure<VariableExpression> replaceWith
+
+    @Override
+    Expression transform(Expression expression) {
+        if ((expression instanceof VariableExpression) && when(expression)) {
+            VariableExpression newExpression = replaceWith(expression)
+            return newExpression
+        }
+        return expression.transformExpression(this)
+    }
+}
+
---
+++ src/test/org/codehaus/groovy/transform/tailrec/VariableExpressionReplacerTest.groovy
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2013-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.transform.tailrec
+
+import org.codehaus.groovy.ast.ASTNode
+import org.codehaus.groovy.ast.ClassHelper
+import org.codehaus.groovy.ast.Parameter
+import org.codehaus.groovy.ast.builder.AstBuilder
+import org.codehaus.groovy.ast.expr.BinaryExpression
+import org.codehaus.groovy.ast.expr.BooleanExpression
+import org.codehaus.groovy.ast.expr.ClosureExpression
+import org.codehaus.groovy.ast.expr.ConstantExpression
+import org.codehaus.groovy.ast.expr.Expression
+import org.codehaus.groovy.ast.expr.VariableExpression
+import org.codehaus.groovy.ast.stmt.*
+import org.codehaus.groovy.syntax.Token
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author Johannes Link
+ */
+class VariableExpressionReplacerTest {
+
+    static final Token EQUALS = Token.newSymbol("==", -1, -1)
+
+    VariableExpressionReplacer replacer
+    def replacements = [:]
+    Closure<Boolean> when = { VariableExpression variableExpression -> replacements.containsKey variableExpression }
+    Closure<VariableExpression> replaceWith = { VariableExpression variableExpression -> replacements[variableExpression] }
+
+    @Before
+    void init() {
+        replacer = new VariableExpressionReplacer(when: when, replaceWith: replaceWith)
+    }
+
+    @Test
+    public void replaceInReturnStatement() {
+        def toReplace = aVariable("old")
+        def replacement = aVariable("new")
+        def returnStatement = new ReturnStatement(toReplace)
+
+        replacements[toReplace] = replacement
+        replacer.replaceIn(returnStatement)
+
+        assert returnStatement.expression == replacement
+    }
+
+    @Test
+    public void replaceEmbeddedInBooleanExpression() {
+        def toReplace = aVariable("old")
+        def replacement = aVariable("new")
+        def returnStatement = new ReturnStatement(new BooleanExpression(toReplace))
+
+        replacements[toReplace] = replacement
+        replacer.replaceIn(returnStatement)
+
+        assert returnStatement.expression.expression == replacement
+    }
+
+
+    @Test
+    public void replaceDeeplyEmbeddedInReturnStatement() {
+        def toReplace = aVariable("old")
+        def replacement = aVariable("new")
+        def returnStatement = new ReturnStatement(new BooleanExpression(new BinaryExpression(toReplace, EQUALS, aConstant('a'))))
+
+        replacements[toReplace] = replacement
+        replacer.replaceIn(returnStatement)
+
+        assert returnStatement.expression.expression.leftExpression == replacement
+    }
+
+
+    def aReturnStatement(value) {
+        new ReturnStatement(aConstant(value))
+    }
+
+    def aConstant(value) {
+        new ConstantExpression(value)
+    }
+
+    def aVariable(value) {
+        new VariableExpression(value)
+    }
+
+    def aBooleanExpression(value) {
+        new BooleanExpression(new ConstantExpression(value))
+    }
+}
